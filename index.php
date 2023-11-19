1e36576a6a8f4b9cf064fa0af0d5bbd4753bf92f
elhindkhalifi <elhindkhalifi@gmail.com>
2023-11-18 23:25:16 -0500

    index marche

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <link rel="stylesheet" href="../styles.css">
+    <title>Document</title>
+</head>
+<body>
+<center>
+<h2>Bienvenue </h2>
+</center>
+<form method="post" action="/formulaires/form1.php">
+        <label for="address">Combien d'adresses avez-vous ?</label>
+        <input type="text" id="address" name="address" required>
+        <br>
+        <input type="submit" value="submit">
+    </form>
+
+</body>
+</html>
